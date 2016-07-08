6fef8118a2ba7669b4dc761a79c8f98a77de53d8
Anderson Araujo <ads.csharp@gmail.com>
2016-07-08 09:26:36 -0300

    Adicionando um component ao projeto

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MeuPrimeiroComponent } from './primeiro/meu-primeiro.component'
+
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Ola Mundo!</p>        
-    `
+        <p>Ola Mundo!</p>
+        <meu-primeiro-component></meu-primeiro-component>
+    `,
+    directives: [MeuPrimeiroComponent]
+
 })
 export class AppComponent { }
